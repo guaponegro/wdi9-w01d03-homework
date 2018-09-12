22ed816b962ed7c251b164593a81d53c5def8685
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-09-12 17:33:59 -0600

    Fizz Buzz answered

--- app.js
+++ app.js
@@ -30,3 +30,43 @@ for(let i = 1; i <= 20; i++){
         }
     }
 }
+
+// Fizz Buzz
+
+// for(i = 1; i <= 100; i++){
+//  if(i % 3 === 0){
+//     console.log("Fizz");
+// } if(i % 5 === 0){
+//     console.log("Buzz");
+// } if(i % 3 === 0 && i % 5 === 0){
+//     console.log("FizzBuzz");
+// }
+// console.log(i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
