02e13709f0fe70ad89427cd40ff1a7b33f2c3be1
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-09-12 17:19:10 -0600

    Excited Kittens answered

--- app.js
+++ app.js
@@ -9,3 +9,24 @@ for(i = 1; i <= 20; i++){
 for(i = 0; i <= 200; i+=2){
     console.log(i);
 }
+
+// Excited Kitten
+letCatTalk = "Love me, pet me! HSSSSSS!"
+letCatTalk2 = "...human...why you taking pictures of me?...";
+letCatTalk3 = "...the catnip made me do it...";
+letCatTalk4 = "...why does the red dot always get away...";
+for(let i = 1; i <= 20; i++){
+    console.log(letCatTalk);
+    if(i % 2 === 0){
+        let testNumber = Math.random();
+        if(testNumber < 0.33){
+            console.log(letCatTalk2);
+        } 
+        if(testNumber > 0.33 && testNumber <= 0.66){
+            console.log(letCatTalk3);
+        }
+        if(testNumber > 0.66){
+            console.log(letCatTalk4);
+        }
+    }
+}
